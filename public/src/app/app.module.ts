9a5422a160ed79bc4643e1ea92a4386dc132ef2b
tintounn <tintounn@gmail.com>
2017-09-06 16:59:44 +0200

    default routing

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {MdButtonModule, MdCardModule, MdInputModule, MdListModule, MdSidenavModule} from "@angular/material";
+import {LoginComponent} from "./pages/login/login.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MdListModule,
+    MdSidenavModule,
+    MdButtonModule,
+    MdInputModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
